1ac91d61be286a45812baa05908dad5e9204a80a
Uwe Karasek <ukarasek@cadcom.de>
2022-05-08 22:11:20 +0200

    log to a file with timestamp

--- main.cpp
+++ main.cpp
@@ -6,24 +6,45 @@
 #include <boost/log/expressions.hpp>
 #include <boost/log/utility/setup/file.hpp> // boost::log::add_file_log
 
+#include <boost/log/core.hpp>
+#include <boost/log/trivial.hpp>
+#include <boost/log/expressions.hpp>
+#include <boost/log/sinks/text_file_backend.hpp>
+#include <boost/log/utility/setup/file.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/sources/severity_logger.hpp>
+#include <boost/log/sources/record_ostream.hpp>
+
+namespace logging = boost::log;
+namespace src = boost::log::sources;
+namespace sinks = boost::log::sinks;
+namespace keywords = boost::log::keywords;
+
 #include <iostream>
 
-#include "main.h"
+// #include "main.h"
 
 void init()
 {
-    boost::log::add_file_log("sample.log");
+    boost::log::add_file_log
+    (
+        boost::log::keywords::file_name = "sample_%N.log"
+        , boost::log::keywords::rotation_size = 10 * 1024 * 1024
+        , boost::log::keywords::time_based_rotation = boost::log::sinks::file::rotation_at_time_point(0, 0, 0)
+        , boost::log::keywords::format = "[%TimeStamp%]: %Message%"
+    );
 
     boost::log::core::get()->set_filter
     (
         boost::log::trivial::severity >= boost::log::trivial::info
     );
+    logging::add_common_attributes();
 }
 
 int main(int argc, char* argv[])
 {
     std::clog << "\nTrivial logging: all log records are written into a file" << std::endl;
-    std::clog <<  "Listed in order of severity from lowest to highest\n" << std::endl;
+    std::clog << "Listed in order of severity from lowest to highest\n" << std::endl;
 
     BOOST_LOG_TRIVIAL(trace) << "A trace severity message";
     BOOST_LOG_TRIVIAL(debug) << "A debug severity message";
@@ -36,9 +57,9 @@ int main(int argc, char* argv[])
     using namespace boost::log;
 
     init();
-    
+
     std::clog << "\nThe first two severities won't pass the filter" << std::endl;
-    std::clog <<  "Listed in order of severity from lowest to highest\n" << std::endl;
+    std::clog << "Listed in order of severity from lowest to highest\n" << std::endl;
 
     BOOST_LOG_TRIVIAL(trace) << "A trace severity message";
     BOOST_LOG_TRIVIAL(debug) << "A debug severity message";
